4091e5a52862a3990ed816006a3ac666c84ddcdd
open-junius <zhou@opentensor.dev>
2025-08-20 19:56:12 +0800

    update key

--- crates/collateral-contract/script/query.sh
+++ crates/collateral-contract/script/query.sh
@@ -1,9 +1,10 @@
-# query
+# basic query to verify the contract is deployed and initialized
 export CONTRACT_ADDRESS=0x970951a12F975E6762482ACA81E57D5A2A4e73F4
+export NETWORK=local
 
-collateral-cli --network local --contract-address $CONTRACT_ADDRESS query trustee
-collateral-cli --network local --contract-address $CONTRACT_ADDRESS query min-collateral-increase
-collateral-cli --network local --contract-address $CONTRACT_ADDRESS query decision-timeout
-collateral-cli --network local --contract-address $CONTRACT_ADDRESS query netuid
+collateral-cli --network $NETWORK --contract-address $CONTRACT_ADDRESS query trustee
+collateral-cli --network $NETWORK --contract-address $CONTRACT_ADDRESS query min-collateral-increase
+collateral-cli --network $NETWORK --contract-address $CONTRACT_ADDRESS query decision-timeout
+collateral-cli --network $NETWORK --contract-address $CONTRACT_ADDRESS query netuid
 
 
