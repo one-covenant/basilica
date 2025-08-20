99395b7455241bf0db8a0da0bba32974630f053e
open-junius <zhou@opentensor.dev>
2025-08-20 19:50:42 +0800

    remove redundant files

--- crates/collateral-contract/script/query-local.sh
+++
@@ -1,9 +0,0 @@
-# query
-export CONTRACT_ADDRESS=0x970951a12F975E6762482ACA81E57D5A2A4e73F4
-
-collateral-cli --network local --contract-address 0xc01Ee7f10EA4aF4673cFff62710E1D7792aBa8f3 query trustee
-collateral-cli --network local --contract-address 0xc01Ee7f10EA4aF4673cFff62710E1D7792aBa8f3 query min-collateral-increase
-collateral-cli --network local --contract-address 0xc01Ee7f10EA4aF4673cFff62710E1D7792aBa8f3 query decision-timeout
-collateral-cli --network local --contract-address 0xc01Ee7f10EA4aF4673cFff62710E1D7792aBa8f3 query netuid
-
-
